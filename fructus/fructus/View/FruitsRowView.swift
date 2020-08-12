8ab2067898f27d551114d0ec738f808db2a1123a
anathayna <anathaynafranca@gmail.com>
2020-08-11 23:05:25 -0300

    adding title content

--- fructus/fructus/View/FruitsRowView.swift
+++ fructus/fructus/View/FruitsRowView.swift
@@ -19,9 +19,20 @@ struct FruitsRowView: View {
                 .scaledToFit()
                 .frame(width: 80, height: 80, alignment: .center)
                 .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
-                .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                )
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
         }
-        .cornerRadius(8)
     }
 }
 
