301489d7dcb97e90f4d17fae040d106bd71f505c
anathayna <anathaynafranca@gmail.com>
2020-08-13 13:27:49 -0300

    adding text details

--- fructus/fructus/App/FruitDetailView.swift
+++ fructus/fructus/App/FruitDetailView.swift
@@ -15,6 +15,19 @@ struct FruitDetailView: View {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .center, spacing: 20) {
                     Text(fruit.title)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy).foregroundColor(fruit.gradientColors[1])
+                    
+                    Text(fruit.headline)
+                        .font(.headline)
+                        .multilineTextAlignment(.leading)
+                    
+                    Text("learn more about \(fruit.title)".uppercased())
+                        .fontWeight(.bold)
+                        .foregroundColor(fruit.gradientColors[1])
+                    
+                    Text(fruit.description)
+                        .multilineTextAlignment(.leading)
                 }
                 .padding(.horizontal, 20)
                 .frame(maxWidth: 640, alignment: .center)
