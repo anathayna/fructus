4b180e31e69e535dc37ab22fa28f677e5009385a
anathayna <anathaynafranca@gmail.com>
2020-08-13 13:34:27 -0300

    adding header contents

--- fructus/fructus/App/FruitHeaderView.swift
+++ fructus/fructus/App/FruitHeaderView.swift
@@ -11,7 +11,18 @@ struct FruitHeaderView: View {
     var fruit: Fruit
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: fruit.gradientColors),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
+        }
+        .frame(height: 440)
     }
 }
 
